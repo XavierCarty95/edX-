2f1e78490bf412d2e2f7ffd89a5dbe2330d4b1ae
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-26 15:19:21 +0000

    modules from edX

---
+++ printgreetings.js
@@ -0,0 +1,9 @@
+var greeting = require('./greeting.js')
+
+console.log('Swedish ' +
+  greeting.sayHelloInSwedish() +
+  ' & English ' +
+  greeting.sayHelloInEnglish() +
+  ' & Tatar ' +
+  greeting.sayHelloInTatar())
+
