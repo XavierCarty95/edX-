c240cb6878f7a2d50f66a8ae6563248bdf016cc1
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-26 16:20:05 +0000

    you can read data from files

--- filesys.js
+++ filesys.js
@@ -1,1 +1,6 @@
-Hey how are you
+const fs = require('fs')
+
+fs.readFile(('greeting.js'), {encoding: 'utf-8'}, function (error, data) {
+  if (error) return console.error(error)
+  console.log(data)
+})
