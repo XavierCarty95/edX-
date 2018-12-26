3b6a798abf24d0515eda9872a41c114137a172e9
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-26 16:25:54 +0000

    join path

---
+++ path.js
@@ -0,0 +1,6 @@
+// const path = require('path');
+// const server = require(path.join('path', 'app1.js'));
+
+const path = require('path')
+const server = require(path.join(__dirname, 'path', 'app1.js')) 
+
