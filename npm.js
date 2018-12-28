c8b492a33c5caf8f66916e9329a59ec922d6dadf
Xavier Carty <xaviercarty.student@careerdevs.com>
2018-12-28 19:08:24 +0000

    npm modules

---
+++ npm.js
@@ -0,0 +1,11 @@
+npm install express
+npm install express@4.2.0
+npm install express@latest
+npm install express --save
+npm install express -S
+npm install mocha --save-dev
+npm install mocha -D
+cat package.json
+ls node_modules/.bin
+npm install express --exact
+npm install express -E
